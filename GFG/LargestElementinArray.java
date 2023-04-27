8abebf3d5b6b5d706aceee4fcc2646bda2f50de0
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-04-28 02:37:18 +0530

    Started Uploading Project

---
+++ GFG/LargestElementinArray.java
@@ -0,0 +1,23 @@
+package GFG;
+public class LargestElementinArray {
+    public static void main(String[] args) {
+        // create an example array
+        int[] arr = {3, 5, 1, 8, 2, 7};
+
+        // find the largest element
+        int largest = largest(arr, arr.length);
+
+        // print the result
+        System.out.println("The largest element is: " + largest);
+    }
+
+    public static int largest(int arr[], int n) {
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > ans) {
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+}
