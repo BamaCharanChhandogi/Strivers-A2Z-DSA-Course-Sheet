ba1e23ba83100b29e888b956ec48d752ecf9ae00
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-05-19 02:35:40 +0530

    A2Z Course

---
+++ SolveProblemsOnArrays/Medium/MajorityElemen.java
@@ -0,0 +1,33 @@
+package SolveProblemsOnArrays.Medium;
+
+public class MajorityElemen {
+    public static void main(String[] args) {
+        int arr[]={2,2,1,1,1,2,2};
+        int candidates=0;
+        int life=1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[candidates]==arr[i]){
+                life++;
+            }
+            else{
+                life--;
+            }
+
+            if(life==0){
+                candidates=i;
+                life=1;
+            }
+        }
+
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[candidates]==arr[i]){
+                count++;
+            }
+        }
+        //check
+        if(count>arr.length/2){
+            System.out.println(arr[candidates]);
+        }
+    }
+}
