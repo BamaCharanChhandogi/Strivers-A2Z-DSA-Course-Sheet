0ca462c5de77e660cd8f94a0256ef55a6612825b
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-05-24 01:26:30 +0530

    new Question

--- SolveProblemsOnArrays/Medium/KadanesAlgorithmmaximumsubarraysum.java
+++ SolveProblemsOnArrays/Medium/KadanesAlgorithmmaximumsubarraysum.java
@@ -2,7 +2,8 @@ package SolveProblemsOnArrays.Medium;
 
 public class KadanesAlgorithmmaximumsubarraysum {
     public static void main(String[] args) {
-        int arr[]={-1,-2,-3,-4};
+        // int arr[]={-1,-2,-3,-4};
+        int arr[]={1,2,3,-2,5};
         int maxsum=arr[0];
         int currsum=0;
         for(int i=0;i<arr.length;i++){
