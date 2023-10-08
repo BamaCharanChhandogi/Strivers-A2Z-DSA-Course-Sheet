aae9639949d21fa9e7aea3ec93a1067492d1a143
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-10-08 13:56:34 +0530

    update

---
+++ Stack_AND_Queues/Valid_Parentheses.java
@@ -0,0 +1,30 @@
+package Stack_AND_Queues;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public static boolean isValid(String s) {
+        int length=s.length();
+        Stack<Character> ans=new Stack<>();
+        for(int i=0;i<length;i++){
+            if(ans.isEmpty()){
+                ans.push(s.charAt(i));
+            }
+            else if((ans.peek()=='(' && s.charAt(i)==')') || (ans.peek()=='{' && s.charAt(i)=='}') || (ans.peek()=='[' && s.charAt(i)==']')){
+                ans.pop();
+
+            }
+            else{
+                ans.push(s.charAt(i));
+            }
+        }
+        if(ans.size()==0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isValid("(){}[]"));
+        System.out.println(isValid("(]"));
+    }
+}
