a768456fc0e352b2f298a38414b080661aedea47
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2023-12-12 23:37:26 +0530

    update

--- Binary_Trees/Medium/Boundary_Traversal.java
+++ Binary_Trees/Medium/Boundary_Traversal.java
@@ -1,46 +1,55 @@
 package Binary_Trees.Medium;
+
 import java.util.*;
+
 public class Boundary_Traversal {
     static List<Integer> ans = new ArrayList<>();
-    public static void BoundryRight(TreeNode root){
-        if(root==null) return;
-        if(root.right!=null){
+
+    public static void BoundryRight(TreeNode root) {
+        if (root == null)
+            return;
+        if (root.right != null) {
             BoundryRight(root.right);
             ans.add(root.val);
-        }
-        else if(root.left!=null){
+        } else if (root.left != null) {
             BoundryRight(root.left);
             ans.add(root.val);
         }
     }
-    public static void BoundryLeft(TreeNode root){
-        if(root==null) return;
-        if(root.left!=null){
+
+    public static void BoundryLeft(TreeNode root) {
+        if (root == null)
+            return;
+        if (root.left != null) {
             ans.add(root.val);
             BoundryLeft(root.left);
-        }
-        else if(root.right!=null){
+        } else if (root.right != null) {
             ans.add(root.val);
             BoundryLeft(root.right);
         }
     }
-    public static void Leaves(TreeNode root){
-        if(root==null) return;
+
+    public static void Leaves(TreeNode root) {
+        if (root == null)
+            return;
         Leaves(root.left);
-        if(root.left==null && root.right==null){
+        if (root.left == null && root.right == null) {
             ans.add(root.val);
         }
         Leaves(root.right);
     }
-    public static void printBoundary(TreeNode root){
-        if(root==null) return;
+
+    public static void printBoundary(TreeNode root) {
+        if (root == null)
+            return;
         ans.add(root.val);
         BoundryLeft(root.left);
         Leaves(root.left);
         Leaves(root.right);
         BoundryRight(root.right);
         System.out.println(ans);
-    }    
+    }
+
     public static void main(String[] args) {
         // You can create a sample tree for testing
         TreeNode root = new TreeNode(3);
