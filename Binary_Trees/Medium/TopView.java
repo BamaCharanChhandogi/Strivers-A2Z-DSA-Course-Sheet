26483c755cefcbcc5073e0d02dc736ac26c18948
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2023-12-13 22:57:41 +0530

    update

---
+++ Binary_Trees/Medium/TopView.java
@@ -0,0 +1,74 @@
+package Binary_Trees.Medium;
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Pair {
+    Node node;
+    int hd;
+
+    public Pair(Node node, int hd) {
+        this.node = node;
+        this.hd = hd;
+    }
+}
+
+class Solution {
+    // Function to return a list of nodes visible from the top view
+    // from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        Queue<Pair> q = new LinkedList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        q.add(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair pair = q.poll();
+            int hd = pair.hd;
+            Node temp = pair.node;
+
+            if (!map.containsKey(hd)) {
+                map.put(hd, temp.data);
+            }
+
+            if (temp.left != null) {
+                q.add(new Pair(temp.left, hd - 1));
+            }
+
+            if (temp.right != null) {
+                q.add(new Pair(temp.right, hd + 1));
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        // Sample usage
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.left.right.right = new Node(5);
+        root.left.right.right.right = new Node(6);
+
+        ArrayList<Integer> result = topView(root);
+
+        System.out.println("Top view of the binary tree: " + result);
+    }
+}
