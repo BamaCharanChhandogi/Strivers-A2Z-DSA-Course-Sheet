52c27296de2dad2b68bfbd12e6638bf7fa27b943
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-02-14 17:33:44 +0530

    add ques

--- Recursion/Get_a_Strong_Hold/Pow_x_n.java
+++ Recursion/Get_a_Strong_Hold/Pow_x_n.java
@@ -1,25 +1,26 @@
 package Recursion.Get_a_Strong_Hold;
-
-import java.text.DecimalFormat;
-
 public class Pow_x_n {
-    public static double myPow(double x, int n) {
-        double result = Math.pow(x, n);
-        
-        // Create a DecimalFormat object to format the result to 5 decimal places
-        DecimalFormat df = new DecimalFormat("#.#####");
-        
-        // Format the result and parse it back to a double
-        double formattedResult = Double.parseDouble(df.format(result));
-        
-        return formattedResult;
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        long  exp = n; // Convert n to long long
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        double result = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1)
+                result *= x;
+            x *= x;
+            exp /= 2;
+        }
+        return result;
     }
-    
     public static void main(String[] args) {
-        double x = 2.3456789;
-        int n = 3;
-        
-        double result = myPow(x, n);
-        System.out.println("Result: " + result);
+        Pow_x_n obj = new Pow_x_n();
+        double x = 2.00000;
+        int n = 10;
+        System.out.println(obj.myPow(x, n));
     }
 }
