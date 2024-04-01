70f9adf13f3b98f420f0313e5cdda79dae920d8d
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-04-01 21:53:50 +0530

    update code

--- Stack_and_Queues/Monotonic_Stack_Queue.java/Next_Greater_Element.java
+++ Stack_and_Queues/Monotonic_Stack_Queue.java/Next_Greater_Element.java
@@ -1,33 +1,38 @@
 import java.util.HashMap;
 
-public class Next_Greater_Element {
-    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        int[] ans = new int[nums1.length];
+
         for (int i = 0; i < nums2.length; i++) {
-            map.put(nums2[i], i);
+            while (!st.isEmpty() && st.peek() < nums2[i]) {
+                nextGreater.put(st.pop(), nums2[i]);
+            }
+            st.push(nums2[i]);
         }
 
-        int ans[] = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            int index = map.get(nums1[i]);
-            ans[i] = -1;
-            for (int j = index + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    ans[i] = nums2[j];
-                    break;
-                }
-            }
+            ans[i] = nextGreater.getOrDefault(nums1[i], -1);
         }
+
         return ans;
     }
 
     public static void main(String[] args) {
-        int nums1[] = { 4, 1, 2 };
-        int nums2[] = { 1, 3, 4, 2 };
-        int arr[] = nextGreaterElement(nums1, nums2);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        Solution solution = new Solution();
+        
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+        
+        int[] result = solution.nextGreaterElement(nums1, nums2);
 
+        System.out.println("Next Greater Elements for nums1:");
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println(nums1[i] + " -> " + result[i]);
+        }
     }
 }
