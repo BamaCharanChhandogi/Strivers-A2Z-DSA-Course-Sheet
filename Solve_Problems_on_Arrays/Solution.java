038de977d2ac8fb16e939d38c41d294dfd0259d4
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-08-05 11:39:57 +0530

    add matrix zero

---
+++ Solve_Problems_on_Arrays/Solution.java
@@ -0,0 +1,55 @@
+package Solve_Problems_on_Arrays;
+import java.util.ArrayList;
+
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        ArrayList<Integer> row = new ArrayList<>();
+        ArrayList<Integer> col = new ArrayList<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < row.size(); i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[row.get(i)][j] = 0;
+            }
+        }
+        for (int i = 0; i < col.size(); i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[j][col.get(i)] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 0, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original matrix:");
+        printMatrix(matrix);
+
+        solution.setZeroes(matrix);
+
+        System.out.println("Matrix after setZeroes:");
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
