7c23427cb9e76da802624eff4805e85088fd9530
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-08-16 23:36:01 +0530

    add optimal approach

--- Binary_Search/BS_on_Answers/Kth_element_of_2_sorted_arrays.java
+++ Binary_Search/BS_on_Answers/Kth_element_of_2_sorted_arrays.java
@@ -1,40 +1,49 @@
 package Binary_Search.BS_on_Answers;
 
 public class Kth_element_of_2_sorted_arrays {
-    public static int kthelement(int arr1[], int arr2[], int m, int n, int k) {
-    if(m > n) {
-        return kthelement(arr2, arr1, n, m, k); 
-    }
-        
-    int low = Math.max(0,k-m), high = Math.min(k,n);
-        
-    while(low <= high) {
-        int cut1 = (low + high) >> 1; 
-        int cut2 = k - cut1; 
-        int l1 = cut1 == 0 ? Integer.MIN_VALUE : arr1[cut1 - 1]; 
-        int l2 = cut2 == 0 ? Integer.MIN_VALUE : arr2[cut2 - 1];
-        int r1 = cut1 == n ? Integer.MAX_VALUE : arr1[cut1]; 
-        int r2 = cut2 == m ? Integer.MAX_VALUE : arr2[cut2]; 
-            
-        if(l1 <= r2 && l2 <= r1) {
-            return Math.max(l1, l2);
+    public static long kthElement(int k, int arr1[], int arr2[]) {
+        // code here
+        int i = 0, j = 0, kth = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                kth++;
+                if (kth == k) {
+                    return arr1[i];
+                }
+                i++;
+            } else {
+                kth++;
+                if (kth == k) {
+                    return arr2[j];
+                }
+                j++;
+            }
         }
-        else if (l1 > r2) {
-            high = cut1 - 1;
+        while (i < arr1.length) {
+            kth++;
+            if (kth == k) {
+                return arr1[i];
+            }
+            i++;
         }
-        else {
-            low = cut1 + 1; 
+        while (j < arr2.length) {
+            kth++;
+            if (kth == k) {
+                return arr2[j];
+            }
+            j++;
         }
+        return -1;
+
     }
-    return -1;
-    }
+
     public static void main(String args[]) {
-    int array1[] = {2,3,6,7,9};
-    int array2[] = {1,4,8,10};
-    int m = array1.length;
-    int n = array2.length;
-    int k = 5;
-    System.out.println("The element at the kth position in the final sorted array is "+kthelement(array1,array2,m,n,k));
-    
+        int array1[] = { 2, 3, 6, 7, 9 };
+        int array2[] = { 1, 4, 8, 10 };
+        int m = array1.length;
+        int n = array2.length;
+        int k = 5;
+        System.out.println(
+                "The element at the kth position in the final sorted array is " + kthElement(k, array1, array2));
     }
 }
